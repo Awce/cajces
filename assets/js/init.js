1b559ea421fa7af932634ccf2ec286dd99ad2d14
Awce <awce5nk@hotmail.com>
2017-03-05 21:09:48 -0600

    Slider

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
-    $('.modal').modal();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+     $('.slider').slider();
+   });
